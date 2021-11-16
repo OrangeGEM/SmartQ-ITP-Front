c148943f195513e5026627c9d5be482f628a5c21
Vitaliy <vitalij.volkov@itp-it.com>
2021-11-16 19:02:44 +0300

    Create pages
    Create LP page and add Router in app.js for routing

---
+++ src/LP/LP.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+
+import styled from "styled-components";
+
+const Conteiner = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center
+`;
+
+export default function LP() {
+    return (
+        <Conteiner>
+            Ololo
+        </Conteiner>
+    )
+}
